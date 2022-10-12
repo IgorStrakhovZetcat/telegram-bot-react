79602c5f33a4a20bd4429a4b5602340ad1815b68
Zetcat <zetcat13@gmail.com>
2022-10-12 13:13:59 +0300

    init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import Button from "../Button/Button";
+
+
+
+
+
+const Header = (props) => {
+    const tg = window.Telegram.WebApp;
+
+
+    const onClose = () => {
+        tg.close()
+      }
+
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Close</Button>
+            <span className={'username'}>
+                {tg.initDataUnsafe?.user?.username}
+            </span>
+        </div>
+    )
+}
+
+export default Header;
