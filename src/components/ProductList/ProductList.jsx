3b388fead258e578d6e87e6b990fd275ad0c3e13
Zetcat <zetcat13@gmail.com>
2022-10-13 12:25:06 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -36,7 +36,7 @@ const ProductList = () => {
         fetch('http://localhost8000', {
             method: 'POST',
             headers: {
-                'Content-Type': 'aplication/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
         })
