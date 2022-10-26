fee4a7e1c3cb0dd383a120178f5390330e2275a3
Zetcat <zetcat13@gmail.com>
2022-10-26 10:48:49 +0300

    init

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -12,9 +12,7 @@ const ProductItem = ({ product, className, onAdd }) => {
         <div className={'product' + className}>
             <img className="img" src={product.img} alt={product.img} />
             
-                <div className="title">{product.title} <div className="price">
-                    <b>{product.cost}$</b>
-                </div> </div>
+                <div className="title">{product.title}   <b>{product.cost}$</b></div>
                 
             
 
